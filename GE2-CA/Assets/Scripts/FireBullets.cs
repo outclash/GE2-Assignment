5ca99cbf5e318b573c8154e84fbf284408928643
Outclash <fjlat96@gmail.com>
2018-03-29 16:43:15 +0100

    update 29/3/18

---
+++ GE2-CA/Assets/Scripts/FireBullets.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullets : MonoBehaviour {
+	
+	public GameObject bulletSpawnPoint;
+	public GameObject bulletPrefab;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab);
+			bullet.transform.position = bulletSpawnPoint.transform.position;
+			bullet.transform.rotation = transform.rotation;
+
+		}
+	}
+}
